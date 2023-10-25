828237997f9267cbb2f06237b3d393e65f14b630
HoTranThienNhan <nhanhotranthien@gmail.com>
2023-10-25 18:07:17 +0700

    Chinh sua cac thay doi

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -5,32 +5,25 @@ const bcrypt = require('bcrypt');
 
 exports.signin = async (req, res, next) => {
     if (!req.body?.email) {
-        return next(new ApiError(400, "Email can not be empty"));
+        return next(new ApiError(400, "Email không được để trống."));
     } 
     if (!req.body?.password) {
-        return next(new ApiError(400, "Password can not be empty"));
+        return next(new ApiError(400, "Mật khẩu không được để trống."));
     } 
 
     try {
         const userService = new UserService(MongoDB.client);
         const { email, password } = req.body;
-        
-        if (!email) {
-            return next(new ApiError(404, "Email is required"));
-        }
-        if (!password) {
-            return next(new ApiError(404, "Password is required"));
-        }
 
         const document = await userService.signin(req.body);
 
         if (!document) {
-            return next(new ApiError(404, "Email or password incorrect"));
+            return next(new ApiError(404, "Tài khoản hoặc mật khẩu không đúng."));
         } else {
             // compare password and password in database
             const comparePassword = bcrypt.compareSync(password, document?.password);
             if (!comparePassword) {
-                return next(new ApiError(404, "Email or password incorrect"));
+                return next(new ApiError(404, "Tài khoản hoặc mật khẩu không đúng."));
             }
         }
 
@@ -44,32 +37,32 @@ exports.signin = async (req, res, next) => {
 
 exports.signup = async (req, res, next) => {
     if (!req.body?.name) {
-        return next(new ApiError(400, "Name can not be empty"));
+        return next(new ApiError(400, "Họ tên không được để trống."));
     } 
     if (!req.body?.email) {
-        return next(new ApiError(400, "Email can not be empty"));
+        return next(new ApiError(400, "Email không được để trống."));
     } 
     if (!req.body?.phone) {
-        return next(new ApiError(400, "Phone can not be empty"));
+        return next(new ApiError(400, "Số điện thoại không được để trống."));
     } 
     if (!req.body?.address) {
-        return next(new ApiError(400, "Address can not be empty"));
+        return next(new ApiError(400, "Địa chỉ không được để trống."));
     } 
     if (!req.body?.password) {
-        return next(new ApiError(400, "Password can not be empty"));
+        return next(new ApiError(400, "Mật khẩu không được để trống."));
     } 
     if (!req.body?.confirmPassword) {
-        return next(new ApiError(400, "Confirm password can not be empty"));
+        return next(new ApiError(400, "Mật khẩu nhập lại không được để trống."));
     } 
     if (req.body?.password !== req.body?.confirmPassword) {
-        return next(new ApiError(400, "Password and confirm password does not match"));
+        return next(new ApiError(400, "Mật khẩu và mật khẩu nhập lại không trùng khớp."));
     }
     
     try {
         const userService = new UserService(MongoDB.client);
         const existedUser = await userService.findByEmail(req.body?.email);
         if (existedUser) {
-            return next(new ApiError(400, "User already exists"));
+            return next(new ApiError(400, "Tài khoản đã tồn tại."));
         }
         const document = await userService.signup(req.body);
         return res.send(document);
@@ -86,12 +79,12 @@ exports.findByEmail = async (req, res, next) => {
         const userService = new UserService(MongoDB.client);
         const document = await userService.findByEmail(req.params.id);
         if (!document) {
-            return next(new ApiError(404, "User not found"));
+            return next(new ApiError(404, "Không tìm thấy tài khoản"));
         }
         return res.send(document);
     } catch (error) {
         return next(
-            new ApiError(500, `Error retrieving user with email=${req.params.id}`)
+            new ApiError(500, `Error retrieving user with email = ${req.params.id}`)
         );
     }
 };
